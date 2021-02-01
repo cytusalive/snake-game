6a42ca342223e75d35bcae8c238a9c1bd9d9ef80
Chocological <cytusomega@gmail.com>
2021-01-31 22:41:01 -0800

    playable

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # snake game work in progress
 
 map class written, snake class written, movement done
+
+basic game features all done, now playable game
--- snake.py
+++ snake.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
+import random
 
 #initialize pygame
 pygame.init()
 pygame.display.set_caption("Snake game")
 
 #initialize screen
-screen = pygame.display.set_mode((600, 600))
+screenx = 600
+screeny = 600
+screen = pygame.display.set_mode((screenx, screeny))
 bgcolor = (230, 230, 100)
 
 #initialize fps
@@ -14,28 +17,43 @@ clock = pygame.time.Clock()
 
 class Field:
     def __init__(self, x, y):
-        self.field = [[0 for a in range(y)] for b in range(x)]
+        self.field = [[0 for a in range(x)] for b in range(y)]
         self.ysize = y
         self.xsize = x
         self.gridsize = screen.get_width() // self.xsize
+        self.eating = False
     
     def drawfield(self):
         for y in range(self.ysize):
             for x in range(self.xsize):
                 if type(self.field[y][x]) == int and self.field[y][x] > 0:
                     pygame.draw.rect(screen, (20,50,200), (x*self.gridsize, y*self.gridsize, self.gridsize, self.gridsize))
+                elif type(self.field[y][x]) == str:
+                    pygame.draw.rect(screen, (200,50,200), (x*self.gridsize, y*self.gridsize, self.gridsize, self.gridsize))
 
     def iterate(self):
+        if self.eating == False:
+            for y in range(self.ysize):
+                for x in range(self.xsize):
+                    if type(self.field[y][x]) == int and self.field[y][x] > 0:
+                        self.field[y][x] -= 1
+        else:
+            self.eating = False
+    
+    def spawnfruit(self):
+        emptyspaces = []
         for y in range(self.ysize):
             for x in range(self.xsize):
-                if type(self.field[y][x]) == int and self.field[y][x] > 0:
-                    self.field[y][x] -= 1
+                if self.field[y][x] == 0:
+                    emptyspaces.append((x, y))
+        spawnx, spawny = emptyspaces[random.randint(0, len(emptyspaces))]
+        self.field[spawny][spawnx] = "@"
 
-game = Field(15, 15)
+game = Field(screenx // 30 , screeny // 30)
 
 class Snake:
     def __init__(self):
-        self.length = 10
+        self.length = 2
         self.xpos = 1
         self.ypos = 1
         self.direction = 1
@@ -47,8 +65,14 @@ class Snake:
         if x in range(game.xsize) and y in range(game.ysize):
             if game.field[y][x] == 0:
                 self.xpos, self.ypos = x, y
-            else:
+            elif type(game.field[y][x]) == int:
                 self.gameover()
+            elif type(game.field[y][x]) == str:
+                self.xpos, self.ypos = x, y
+                game.field[y][x] = 0
+                self.length += 1
+                game.eating = True
+                game.spawnfruit()
         else:
             self.gameover()
     
@@ -59,9 +83,8 @@ class Snake:
         pygame.quit()
         print("Game Over, your length:", self.length)
 
-
 snake = Snake()
-
+game.spawnfruit()
 
 while True:
     screen.fill(bgcolor)
@@ -91,4 +114,4 @@ while True:
     snake.update()
     game.drawfield()
     pygame.display.update()
-    msElapsed = clock.tick(4)
+    msElapsed = clock.tick(5)
